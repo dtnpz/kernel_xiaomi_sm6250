7aa8f6b026a7658928c4a514fe07e9831a46668e
dtnpz <36336494+dtnpz@users.noreply.github.com>
2026-09-19 10:17:06 +0700

    show KSUN identity in AnyKernel installer

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=gxter-Kernel by maintainer zared
+kernel.string=Gxterkernl joyeuse rev187 KSUN by zared
 do.devicecheck=1
 do.modules=0
 do.systemless=1
